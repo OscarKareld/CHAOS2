fd8e1f6a05675dd965f9300eff687b4cf8df9a57
Anni Johansson <annij@live.se>
2021-04-27 16:14:34 +0200

    Updated script health bar

--- CHAOS2_Unity_Project/Assets/Scripts/HealthBar.cs
+++ CHAOS2_Unity_Project/Assets/Scripts/HealthBar.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] public Slider slider;
+
+    public void SetHealth(int health)
     {
-        
+        slider.value = health;
+
     }
 }
+

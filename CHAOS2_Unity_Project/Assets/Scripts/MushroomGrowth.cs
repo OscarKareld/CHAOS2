6a8d549f1a8d759a8e38a748547302b5481c38a2
OscarKareld <oscar.kareld@gmail.com>
2021-04-27 16:01:09 +0200

    Växande svamp

---
+++ CHAOS2_Unity_Project/Assets/Scripts/MushroomGrowth.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomGrowth : MonoBehaviour
+{
+    public float x = 1;
+    public float y = 0;
+    public float z = 1;
+
+    public int counter = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if ((counter % 100) == 0) {
+            transform.localScale = new Vector3(x++, y++, z++);
+        }
+    }
+}

52b55cf6bc6c9d4b1ed65dcad3a24ac2fdc051cc
SofiaMHallberg <62309798+SofiaMHallberg@users.noreply.github.com>
2021-04-27 22:13:18 +0200

    Körbar bil
    Nu kan bilen köra framåt , bakåt och svänga!

--- CHAOS2_Unity_Project/Assets/Scripts/CarController.cs
+++ CHAOS2_Unity_Project/Assets/Scripts/CarController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    public float speed;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +18,11 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.Translate(0,0,1);
-        transform.Translate(Vector3.forward);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward*Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }

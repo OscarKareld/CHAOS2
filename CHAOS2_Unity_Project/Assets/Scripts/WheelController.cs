142cffaef44be79dca54f07aa43838c2441c9be1
Chanon Borgström <dborgstroem@gmail.com>
2021-04-27 16:05:18 +0200

    updated wheel

--- CHAOS2_Unity_Project/Assets/Scripts/WheelController.cs
+++ CHAOS2_Unity_Project/Assets/Scripts/WheelController.cs
@@ -8,6 +8,6 @@ public class WheelController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(10f, 0f,0f * Time.deltaTime);
+        transform.Rotate(0f, 10f,0f * Time.deltaTime);
     }
 }
